fc4804f770379b13fa3814afebc7937e45b5d659
Will Beckman <wbeckman9@gmail.com>
2025-02-09 20:46:31 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className="text-xl font-bold text-blue-300">This website is currently under active development. Check back again soon!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
