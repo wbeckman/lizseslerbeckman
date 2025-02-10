c99140565bdf9f940e696cfbebaf9496d90619d1
Will Beckman <wbeckman9@gmail.com>
2025-02-09 21:08:46 -0500

    small bug fix with assets path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lizseslerbeckman/',
+  base: '/',
   plugins: [react(), tailwindcss(),],
 })
